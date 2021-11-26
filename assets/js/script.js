a80d4287448c76cd89c01f507c46568ddb5420cc
luis fernando <fer.almeidabr@hotmail.com>
2021-11-25 21:22:12 -0300

    Atualização no html e css | criação do js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 let nome = document.querySelector('#nome')
 let nomeOk = false
+let email = document.querySelector('#email')
+let emailOk = false
+let comentario = document.querySelector('#comentario')
+let comentarioOk = false
+
+nome.style.width = '100%'
+email.style.width = '100%'
 
 //função para validar o nome 
 function validarNome() {
@@ -12,13 +19,40 @@ function validarNome() {
         txtNome.style.color = 'green'
         nomeOk = true
     }
-} 
+}
+
+function validarEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if (email.value.indexOf('@' && '.') == -1) {
+        txtEmail.innerHTML = 'Email inválido'
+        txtEmail.style.color = 'red'
+    }else{
+        txtEmail.innerHTML = 'Tudo certo'
+        txtEmail.style.color = 'green'
+        emailOk = true
+    }
+    
+}
+
+function validarComentario() {
+    let txtComentario = document.querySelector('#txtComentario')
+    if(comentario.value.length < 100) {
+        txtComentario.innerHTML = 'Comentario valido'
+        txtComentario.style.color = 'green' 
+    }else{
+        txtComentario.innerHTML = 'O comentario não pode ultrapassar 100 caracteres'
+        txtComentario.style.color = 'red'
+        comentarioOk = true
+    }
+}
+
 
 // função para enviar o nome
 function enviar(){
-    if(nomeOk == true){
+    if(nomeOk == true || emailOk == true || comentarioOk == true){
         alert('Enviado')
     }else{
-        alert('preencha o nome corretamente!')
+        alert('Preencha os dados corretamente')
     }
 }
